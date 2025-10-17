af5aa341e38b4321fb5b34ea5b65ea50ddc6cd64
Aslly Zuñiga <asllyz2610@gmail.com>
2025-10-17 09:57:43 -0500

    Cambios en paginas de los niveles

--- src/App.tsx
+++ src/App.tsx
@@ -39,9 +39,9 @@ function App() {
           
           {/* Rutas principales */}
           <Route path="/" element={<Dashboard />} />
-          <Route path="/nivel1" element={<Prueba nivel={1} imagen="/niveles/nivel1.svg" lecturas={4} />} />
-          <Route path="/nivel2" element={<Prueba nivel={2} imagen="/niveles/nivel2.svg" lecturas={6} />} />
-          <Route path="/nivel3" element={<Prueba nivel={3} imagen="/niveles/nivel3.svg" lecturas={8} />} />
+          <Route path="/nivel1" element={<Prueba nivel={1} imagen="/niveles/nivel1.webp" lecturas={4} />} />
+          <Route path="/nivel2" element={<Prueba nivel={2} imagen="/niveles/nivel2.webp" lecturas={6} />} />
+          <Route path="/nivel3" element={<Prueba nivel={3} imagen="/niveles/nivel3.webp" lecturas={8} />} />
           <Route path="/nivel4" element={<Prueba nivel={4} imagen="/niveles/nivel4.svg" lecturas={8} />} />
         </Routes>
       </Suspense>
--- src/components/ageCircle/AgeCircle.tsx
+++ src/components/ageCircle/AgeCircle.tsx
@@ -1,41 +1,28 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 import "./AgeCircle.scss";
-import Camara from "../../assets/img/camara.svg";
-import Cuaderno from "../../assets/img/cuaderno.svg";
-import PaletaColores from "../../assets/img/paletaColores.svg";
+
+import Btn1 from "../../assets/img/btn_7-8.webp";
+import Btn2 from "../../assets/img/btn-9-10.webp"; 
+import Btn3 from "../../assets/img/btn-11-12.webp"; 
 
 export default function AgeCircle() {
   const navigate = useNavigate();
-  const [clickedIndex, setClickedIndex] = useState<number | null>(null);
 
-  const circles = [
-    { age: "7 - 8 años", image: Camara, to: "/nivel1" },
-    { age: "9 - 10 años", image: Cuaderno, to: "/nivel2" },
-    { age: "11 - 12 años", image: PaletaColores, to: "/nivel3" },
+  const buttons = [
+    { age: "7 - 8 años", image: Btn1, to: "/nivel1" },
+    { age: "9 - 10 años", image: Btn2, to: "/nivel1" },
+    { age: "11 - 12 años", image: Btn3, to: "/nivel3" },
   ];
 
-  const handleClick = (to: string, index: number) => {
-    setClickedIndex(index);
-    setTimeout(() => {
-      navigate(to); 
-    }, 250); 
-  };
-
   return (
-    <div className="age-circles">
-      {circles.map((circle, i) => (
+    <div className="age-buttons">
+      {buttons.map((btn, i) => (
         <div
           key={i}
-          className={`age-circle color-${i + 1} ${clickedIndex === i ? "clicked" : ""}`}
-          onClick={() => handleClick(circle.to, i)}
+          className="age-button"
+          onClick={() => navigate(btn.to)}
         >
-          <img
-            src={circle.image}
-            alt={circle.age}
-            className="age-circle__icon"
-          />
-          <span className="age-circle__text">{circle.age}</span>
+          <img src={btn.image} alt={btn.age} className="age-button__img" />
         </div>
       ))}
     </div>
